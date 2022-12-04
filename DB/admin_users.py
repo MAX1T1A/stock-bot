81fca1bcadf001afd4a3e057fbc2b88a7ab270b7
max1t1a <max1t1aldan@gmail.com>
2022-12-05 00:13:15 +0300

    commit

--- DB/admin_users.py
+++ DB/admin_users.py
@@ -1,5 +1,5 @@
 def user_check_on_admin(user_id):
-    if user_id is ("1172084194", "1041232059"):
+    if user_id in ("1172084194", "1041232059"):
         return True
     else:
         return False
